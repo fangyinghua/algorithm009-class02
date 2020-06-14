94ea4f311b1ec60a64475cb5ddcb05c1598fb2bc
fangyinghua <1529233641@qq.com>
2020-06-14 23:44:51 +0800

    第四周

--- Week_04/search-in-rotated-sorted-array.js
+++ Week_04/search-in-rotated-sorted-array.js
@@ -5,23 +5,23 @@
  */
 var search = function(nums, target) {
 
-  let left = 0, numLen = nums.length - 1, mid = 0;
-  while (left <= numLen) {
-      mid = (left + (numLen - left) / 2) |0;
+  let left = 0, right = nums.length - 1, mid = 0;
+  while (left <= right) {
+      mid = (left + (right - left) / 2) |0;
       if (nums[mid] == target) {
           return mid;
       }     
-      if (nums[mid] >= nums[left]) {         
+      if (nums[left]<=nums[mid]) {//单调递增  
           if (target >= nums[left] && target < nums[mid]) {
-              numLen = mid - 1;
+             right = mid - 1;
           } else {
-              left = mid + 1;
+             left = mid + 1;
           }
       } else {
-          if (target > nums[mid] && target <= nums[hi]) {
+          if (target > nums[mid] && target <= nums[right]) {
               left = mid + 1;
           } else {
-              numLen = mid - 1;
+             right = mid - 1;
           }
       }
   }
